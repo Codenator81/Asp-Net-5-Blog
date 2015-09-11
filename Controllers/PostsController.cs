8812d235e12c85e0c86bb3ddbfa09c1e2d2a6edf
Codenator81 <codenator81@gmail.com>
2015-09-11 23:57:30 +0300

    Add validation

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -17,12 +17,25 @@ namespace AspNetBlog.Controllers
 
         public IActionResult Post(long id)
         {
-            var post = new Post();
-
-            post.Title = "My Blog Post";
-            post.PostedDate = DateTime.Now;
-            post.Author = "Alex Poltarjonoks";
-            post.Body = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+            var post = new Post
+            {
+                Title = "My Blog Post",
+                PostedDate = DateTime.Now,
+                Author = "Alex Poltarjonoks",
+                Body =
+                    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy " +
+                    "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, " +
+                    "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea " +
+                    "rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem " +
+                    "ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, " +
+                    "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," +
+                    " sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " +
+                    "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet." +
+                    " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy " +
+                    "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam " +
+                    "voluptua. At vero eos et accusam et justo duo dolores et ea rebum. " +
+                    "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+            };
 
             return View(post);
         }
