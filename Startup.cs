57f9858ba2438f5c68740c73a646003d7c2f9cd5
Codenator81 <codenator81@gmail.com>
2015-09-12 22:56:42 +0300

    Configuration management in Json format

--- Startup.cs
+++ Startup.cs
@@ -8,12 +8,25 @@ using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Data.Entity;
+using Microsoft.Framework.Configuration;
+using Microsoft.Dnx.Runtime;
 
 namespace AspNetBlog
 {
     public class Startup
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfigurationRoot config { get; set; }
+
+        public Startup(IApplicationEnvironment env)
+        {
+            config = new ConfigurationBuilder(env.ApplicationBasePath)
+                .AddEnvironmentVariables()
+                .AddJsonFile("config.json")
+                .AddJsonFile("config.dev.json", true)
+                .AddUserSecrets()
+                .Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             var connectPostgres = "Host=localhost;Username=postgres;Password=root;Database=asp_net_blog";
@@ -27,10 +40,17 @@ namespace AspNetBlog
 
         public void Configure(IApplicationBuilder app)
         {
-            app.UseErrorPage();
-            app.UseRuntimeInfoPage();
+            var password = config["password"];
+            if (config["debug"] == "true")
+            {
+                app.UseErrorPage();
+                app.UseRuntimeInfoPage();
+            }
+            else
+            {
+                app.UseErrorHandler("/home/error");
+            }
 
-            app.UseErrorHandler("/home/error");
             app.UseMvc(routes =>
                 routes.MapRoute("Defoult",
                 "{controller=Home}/{action=Index}/{id?}"));
