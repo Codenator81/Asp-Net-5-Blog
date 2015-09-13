2f6d4ea2aee0ce71b3cad0f5300a7b1e48f56df6
Codenator81 <codenator81@gmail.com>
2015-09-13 14:23:08 +0300

    Add login and register pages with controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Identity;
+using AspNetBlog.Models.Identity;
+using AspNetBlog.Models.Account;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetBlog.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+                                 SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel login, string returnUrl = null)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(
+                login.EmailAddress,
+                login.Password,
+                login.RememberMe,
+                false);
+
+            if (!result.Succeeded)
+            {
+                return View(login);
+            }
+
+            if (string.IsNullOrWhiteSpace(returnUrl))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return Redirect(returnUrl);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+
+            if (string.IsNullOrWhiteSpace(returnUrl))
+                return RedirectToAction("Index", "Home");
+
+            return Redirect(returnUrl);
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registration)
+        {
+            if (!ModelState.IsValid)
+                return View(registration);
+
+            var newUser = new ApplicationUser
+            {
+                Email = registration.EmailAddress,
+                UserName = registration.EmailAddress,
+            };
+
+            var result = await _userManager.CreateAsync(newUser, registration.Password);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Error creating user");
+                return View(registration);
+            }
+
+            return RedirectToAction("Login");
+        }
+
+    }
+}
